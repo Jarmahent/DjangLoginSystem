9f6ba74f44dda2c55da5eb43f32e9c6462317597
Kevin <kjh7796@gmail.com>
2018-07-02 21:15:14 -0400

    Created test for database queries

--- login/apps.py
+++ login/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+from django.db import models
 
 class LoginConfig(AppConfig):
     name = 'login'
--- login/models.py
+++ login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class user_info(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
