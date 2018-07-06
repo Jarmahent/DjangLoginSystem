577636ca7a211cf0d63af4516e9a9171c3805816
= <=>
2018-07-06 15:22:36 -0400

    Fixed forms issues for items

--- storefront/forms.py
+++ storefront/forms.py
@@ -1,14 +1,14 @@
-from django.forms import ModelForm
 from django import forms
 from storefront.models import store_item
 
-class Itemform(forms.Form):
+class Itemform(forms.ModelForm):
     item_name = forms.CharField(label="Item Name" ,max_length=200)
+    item_description = forms.CharField(label="Item Description" ,max_length=500, widget=forms.Textarea)
     item_price = forms.DecimalField(label="Item Price", max_value=500, decimal_places=3)
     item_is_available = forms.TypedChoiceField(choices=((False, 'No'), (True, 'Yes')))
 
 
-class NewItemForm(ModelForm):
+class ItemModelForm(Itemform):
     class Meta:
         model = store_item
         field = ['item_name', 'item_price', 'item_is_available', 'item_description']
--- storefront/views.py
+++ storefront/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse, HttpResponseRedirect
 from storefront.models import store_item
 from django.contrib.auth.decorators import user_passes_test
-from storefront.forms import NewItemForm
+from storefront.forms import ItemModelForm
 
 def store(request):
     items = list(store_item.objects.values())
@@ -11,10 +11,16 @@ def store(request):
 @user_passes_test(lambda u: u.is_superuser)
 def submit_item(request):
     if request.method == 'POST':
-        form = NewItemForm(request.POST)
+        form = ItemModelForm(request.POST)
         if form.is_valid():
             form.save()
+            print({
+                "item_name": form.cleaned_data.get('item_name'),
+                "item_description": form.cleaned_data.get('item_description'),
+                "item_price": form.cleaned_data.get('item_price'),
+                "item_is_available": form.cleaned_data.get('item_is_available')
+            })
             return HttpResponseRedirect('/store')
     else:
-        form = NewItemForm()
+        form = ItemModelForm()
     return render(request, 'storefront/submit_item.html', {"form": form})
