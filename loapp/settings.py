f0283e4fd7d43c7fdb7b5b62f2d38705babbb16a
Kevin <kjh7796@gmail.com>
2018-07-07 19:27:54 -0400

    Added image file uploading to store front template

--- loapp/settings.py
+++ loapp/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/homepage'
 LOGOUT_REDIRECT_URL = '/accounts/login'
--- storefront/models.py
+++ storefront/models.py
@@ -6,10 +6,10 @@ from django.db import models
 class store_item(models.Model):
     item_id = models.AutoField(primary_key=True)
     item_name = models.CharField(max_length=200)
-    item_price = models.IntegerField()
+    item_price = models.DecimalField(decimal_places=2, max_digits=10)
     item_is_available = models.BooleanField(default=True)
     item_description = models.CharField(max_length=500)
-    item_image = models.FileField(upload_to="storefront/static/storefront/media", default='storefront/media')
+    item_image = models.FileField(upload_to="", default='media')
 
     def __str__(self):
         return self.item_name
