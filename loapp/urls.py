63f4384b761fef875bf2949347cd2cc5123cf178
= <=>
2018-07-13 08:33:53 -0400

    Refactoring password_reset forms

--- loapp/urls.py
+++ loapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import password_change, password_change_done
+from django.contrib.auth.views import password_change, password_change_done, password_reset
 
 urlpatterns = [
     path('admin/', admin.site.urls),
