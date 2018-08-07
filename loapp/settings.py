cc00e74e4c984525a69e98afbcd9ebd57d340bd7
Kevin <kjh7796@gmail.com>
2018-08-06 20:49:52 -0400

    Removed personal info

--- loapp/settings.py
+++ loapp/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kevin@kevintweaks.net'
-EMAIL_HOST_PASSWORD = 'Laurh2335'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
